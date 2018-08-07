16d6b440af96ecab601819b9690a55ce5f8814eb
Carlos <carlos.x71@hotmail.com>
2018-08-07 14:16:12 -0300

    Criação do Servidor

--- SisAss/server.js
+++ SisAss/server.js
@@ -1,3 +1,28 @@
 'use strict' //Força o JavaScript a ser critérioso (ponto e virgula e etc)
 
-console.log('Testando...');
+//Importando pacotes (Elas vem da pasta node_modules)
+const http = require('http');
+//const debug = require('debug')('SisAss:server'); //olha aula 3 qualquer coisa
+const express = require('express');
+
+//configurando a porta do servidor
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+//Instânciando o servidor
+const server = http.createServer(app);
+const router = express.Router();
+
+//Rotas
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+});
+
+app.use('/', route);
+
+server.listen(port); //aonde aponta e fica ouvindo a porta que definimos
+console.log('Ta funcionando carai!!!!! '+ port);
