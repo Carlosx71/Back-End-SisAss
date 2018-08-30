6aaad0aec0970f0cfee2c509b289c0a0d110a7d9
Carlos <carlos.x71@hotmail.com>
2018-08-30 12:53:53 -0300

    Conexão com o banco mongoDB

--- SisAss/src/app.js
+++ SisAss/src/app.js
@@ -3,9 +3,14 @@
 //Importando pacotes (Elas vem da pasta node_modules)
 const express = require('express');
 const bodyParser = require('body-Parser'); //Este pacote converte o corpo da requisição para json
-
+const mongoose = require('mongoose'); //Instalei um pacote via npm na aula 14
 const app = express();
 const router = express.Router();
+
+//Conecta ao banco aula 14
+//mongoose.connect('mongodb://siassmaster:siass4321@ds018508.mlab.com:18508/siassmaster');
+//Conecta ao banco aula 14 o conjunto { useNewUrlParser: true } coloquei, pois sem isso a conexão não valida depois de um tempo
+mongoose.connect("mongodb://siassmaster:siass4321@ds018508.mlab.com:18508/siassmaster", { useNewUrlParser: true });
 
 //carregar rotas que estão em index
 const indexRoute = require('./routes/index-route');
